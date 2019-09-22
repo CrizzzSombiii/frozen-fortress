9ad9e1d0315b76d3f308acfdcb45fed58e84bb7b
DrCrizzSombii <cristianstein555@gmail.com>
2019-09-22 14:27:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ mongojs=require("mongojs")
 db=mongojs("ds117590.mlab.com:17590/mc2db",{"account","progress"])
 app=require("express")()
 net=require("http").createServer(app)
-app.get("/",(req,res)=>{
-res.sendFile(__dirname+"/public/index.htm")
-})
 app.use("/public",require("express").static(__dirname+"/public"))
 net.listen(process.env.PORT||3000,()=>{console.log("Ready!")})
 require("socket.io")(net,{}).sockets.on("connection",(socket)=>{
